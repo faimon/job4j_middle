ef2ebd53ba6fb5a8b6e991665c9a6a8d6acaaf92
andreykatanov <faimon96@gmail.com>
2020-09-26 17:07:30 +0700

    4. Скачивание файла с ограничением. [#373057]

---
+++ middle/src/main/java/net/FileDownload.java
@@ -0,0 +1,33 @@
+package net;
+
+import java.io.*;
+import java.net.URL;
+
+public class FileDownload {
+    public void fileDownload(String url, String maxSpeed, String directory) {
+        int speedKb = Integer.parseInt(maxSpeed) * 1024;
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(directory)) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            int timePause;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                Thread.sleep(1000);
+                System.out.println("bytes read: " + bytesRead);
+                if (bytesRead > speedKb) {
+                    timePause = (bytesRead - speedKb) / speedKb;
+                    System.out.println("pause time: " + timePause);
+                    Thread.sleep(timePause);
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        FileDownload fileDownload = new FileDownload();
+        fileDownload.fileDownload(args[0], args[1], "test.txt");
+    }
+}

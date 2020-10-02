28f64f3e0d8885f4f8a18b3f210cbcedd82df0be
andreykatanov <faimon96@gmail.com>
2020-10-02 14:00:29 +0700

    3. Junit тест для блокирующей очереди. [#373053]

--- middle/src/test/java/wait/SimpleBlockingQueueTest.java
+++ middle/src/test/java/wait/SimpleBlockingQueueTest.java
@@ -2,6 +2,10 @@ package wait;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
@@ -29,5 +33,31 @@ public class SimpleBlockingQueueTest {
         producer.join();
         consumer.start();
         consumer.join();
+    }
+
+    @Test
+    public void whenTwoThread() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(6);
+        Thread producer = new Thread(
+                () -> {
+                    IntStream.range(0, 5).forEach(
+                            queue::offer
+                    );
+                }
+        );
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        buffer.add(queue.poll());
+                    }
+                }
+        );
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
     }
 }

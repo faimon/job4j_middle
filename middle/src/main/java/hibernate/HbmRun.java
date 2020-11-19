76bb01ebd48872abe5846340abd6975764717228
mac <faimon96@gmail.com>
2020-11-19 20:41:02 +0700

    test hibernate @OneToMany

---
+++ middle/src/main/java/hibernate/HbmRun.java
@@ -0,0 +1,36 @@
+package hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HbmRun {
+
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try {
+            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Session session = sf.openSession();
+            session.beginTransaction();
+
+            User user = User.of("Ivan");
+            session.save(user);
+
+            Role admin = Role.of("moderator");
+            admin.addUser(session.load(User.class, user.getId()));
+
+
+            session.save(admin);
+
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
